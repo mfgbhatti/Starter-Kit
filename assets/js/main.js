8d33b0442aa9c52b5519a309cb0b421ee9fbe91f
mfgbhatti <mfgbhatti@live.com>
2017-09-01 01:26:53 +0100

    version 1.0.1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,11 @@
-
+$(function (){
+  mfgbhatti.Init();
+});
+var mfgbhatti = {
+  'Actions' : new Object(),
+  'Objects' : new Object(),
+  'Variables' : new Object(),
+  'Init': function() {
+//adding new functions as it
+    }
+};
